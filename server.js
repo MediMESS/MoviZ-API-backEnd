99f934778f7e47aacf37d7cb1763e564b349d8f6
MediMESS <medimessarat@gmail.com>
2020-03-19 18:22:22 +0100

    finalizing back end

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const knex = require('knex');
-const bcrypt = require('bcrypt');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const knex = require("knex");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
 const fetch = require("node-fetch");
 const unirest = require("unirest");
 
-const register = require('./controllers/register');
-const signIn = require('./controllers/signIn');
-const moviz = require('./controllers/Moviz');
-const movizCard = require('./controllers/MovizCard');
-const like = require('./controllers/Like');
+const register = require("./controllers/register");
+const signIn = require("./controllers/signIn");
+const moviz = require("./controllers/Moviz");
+const movizCard = require("./controllers/MovizCard");
+const like = require("./controllers/Like");
 // const db = knex({
 //   client: 'pg',
 //   connection: {
@@ -19,12 +19,12 @@ const like = require('./controllers/Like');
 //   }
 // });
 const db = knex({
-  client: 'pg',
+  client: "pg",
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: 'postgresqlpassword',
-    database: 'moviz'
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "postgresqlpassword",
+    database: "moviz"
   }
 });
 let PORT = process.env.PORT || 4000;
@@ -33,18 +33,34 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.status(200).json(`ITs working`)
-  console.log('nodemon and server running!');
-})
+app.get("/", (req, res) => {
+  res.status(200).json(`ITs working`);
+  console.log("nodemon and server running!");
+});
 
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
-app.post('/signIn', (req, res) => {signIn.handleSignIn(req, res, db, bcrypt)});
-app.post('/searchMoviz', (req, res) => {moviz.searchMoviz(req, res, fetch, unirest)});
-app.post('/insertMovie', (req, res) => {movizCard.insertMovie(req, res, db)});
-app.delete('/deleteMovie/:movieId', (req, res) => {movizCard.deleteMovie(req, res, db)});
-app.get('/getRating/:movieId', (req, res) => {moviz.getRating(req, res, unirest)});
-app.get('/getLike/:movieId', (req, res) => {movizCard.getLike(req, res, db)});
-app.get('/getMoviesLiked', (req, res) => {like.getMoviesLiked(req, res, db)});
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.post("/signIn", (req, res) => {
+  signIn.handleSignIn(req, res, db, bcrypt);
+});
+app.post("/searchMoviz", (req, res) => {
+  moviz.searchMoviz(req, res, fetch, unirest);
+});
+app.post("/insertMovie", (req, res) => {
+  movizCard.insertMovie(req, res, db);
+});
+app.delete("/deleteMovie/:movieId", (req, res) => {
+  movizCard.deleteMovie(req, res, db);
+});
+app.get("/getRating/:movieId", (req, res) => {
+  moviz.getRating(req, res, unirest);
+});
+app.get("/getLike/:movieId", (req, res) => {
+  movizCard.getLike(req, res, db);
+});
+app.get("/getMoviesLiked", (req, res) => {
+  like.getMoviesLiked(req, res, db);
+});
 
 app.listen(PORT);
